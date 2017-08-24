97b854cb35568ba6b844c991ae4b64da9e31dc09
Spencer Carli <spencercarli@gmail.com>
2017-08-24 11:52:31 -0500

    remove useless files

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTErrorInfo.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTErrorInfo.m \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTErrorInfo.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTJSStackFrame.h
--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/RCTClipboard.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Modules/RCTClipboard.m \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Modules/RCTClipboard.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTBridgeModule.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTDefines.h
