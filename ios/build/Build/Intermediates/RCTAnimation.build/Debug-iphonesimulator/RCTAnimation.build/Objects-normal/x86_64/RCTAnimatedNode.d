7dac96df11335276c5ce02f7675853e724586eb9
Spencer Carli <spencercarli@gmail.com>
2017-08-24 11:55:56 -0500

    remove useless files

--- ios/build/Build/Intermediates/RCTAnimation.build/Debug-iphonesimulator/RCTAnimation.build/Objects-normal/x86_64/RCTAnimatedNode.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTAnimatedNode.m \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTAnimatedNode.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h
--- ios/build/Build/Intermediates/RCTAnimation.build/Debug-iphonesimulator/RCTAnimation.build/Objects-normal/x86_64/RCTValueAnimatedNode.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTValueAnimatedNode.m \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTValueAnimatedNode.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTAnimatedNode.h
