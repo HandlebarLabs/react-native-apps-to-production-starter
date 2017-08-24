6925f74eefc71fe0db23c4c1897a22067f4861ef
Spencer Carli <spencercarli@gmail.com>
2017-08-24 11:54:22 -0500

    remove useless files

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTReloadCommand.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTReloadCommand.m \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTReloadCommand.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/Base/RCTKeyCommands.h
--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/third-party.build/Objects-normal/x86_64/demangle-7B942820D95A2665.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/third-party/glog-0.3.4/src/demangle.cc \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/../third-party/glog-0.3.4/src/demangle.h \
-  /Users/spencer/dev/react-native-apps-to-production-starter/node_modules/react-native/React/../third-party/glog-0.3.4/src/config.h
