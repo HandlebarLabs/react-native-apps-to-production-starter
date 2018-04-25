6686087abc3337c239df3e23b4a5dbdc443bfc69
Spencer Carli <spencercarli@gmail.com>
2018-04-25 12:01:19 -0500

    Remove dep on moment

--- app/components/Text/LastConverted.js
+++ app/components/Text/LastConverted.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import moment from 'moment';
+import { formatDate } from '../../util/dates';
 
 import styles from './styles';
 
@@ -8,7 +8,7 @@ const LastConverted = ({
   date, base, quote, conversionRate,
 }) => (
   <Text style={styles.smallText}>
-    1 {base} = {conversionRate} {quote} as of {moment(date).format('MMMM D, YYYY')}
+    1 {base} = {conversionRate} {quote} as of {formatDate(date)}
   </Text>
 );
 
