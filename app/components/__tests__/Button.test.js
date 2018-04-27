fa352047ccb247e1d2581ed8d21410926768c519
Spencer Carli <spencercarli@gmail.com>
2018-04-27 11:22:22 -0500

    Remove sinon, use jest.fn()

--- app/components/__tests__/Button.test.js
+++ app/components/__tests__/Button.test.js
@@ -3,7 +3,6 @@ import renderer from 'react-test-renderer';
 import 'react-dom';
 import { TouchableOpacity } from 'react-native';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 
 import { ClearButton, styles } from '../Button';
 
@@ -22,7 +21,7 @@ it('renders custom text pass via props', () => {
 });
 
 it('handles a press', () => {
-  const callback = sinon.spy();
+  const callback = jest.fn();
   const wrapper = shallow(<ClearButton text="test 1" onPress={callback} />);
 
   expect(wrapper.find(TouchableOpacity).length).toBe(1);
@@ -32,5 +31,5 @@ it('handles a press', () => {
     .props()
     .onPress();
 
-  expect(callback.called).toBe(true);
+  expect(callback.mock.calls.length).toBe(1);
 });
